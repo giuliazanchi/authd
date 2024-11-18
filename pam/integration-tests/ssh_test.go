2a41522e1a83fe86e442c87b728f7f70bc19d84f
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-11-18 15:41:29 +0100

    pam/integration-tests/ssh: Run tests with noble SSH server and sync golden

--- pam/integration-tests/ssh_test.go
+++ pam/integration-tests/ssh_test.go
@@ -48,7 +48,7 @@ func testSSHAuthenticate(t *testing.T, sharedSSHd bool) {
 		t.Skip("Skipping tests with external dependencies as requested")
 	}
 
-	if uv := getUbuntuVersion(t); uv == 0 || uv >= 2404 {
+	if uv := getUbuntuVersion(t); uv == 0 || uv < 2404 {
 		require.Empty(t, os.Getenv("GITHUB_REPOSITORY"),
 			"Golden files needs to be updated to ensure CI runs on Ubuntu %v")
 		t.Skipf("Skipping SSH tests since they require new golden files for Ubuntu %v", uv)
